4f8d28887994d18122a182cd5d5bf6b7c72c2165
TicTakTok1 <66017543+TickerDev@users.noreply.github.com>
2026-03-27 19:16:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    tanstackStart({
-      spa: {
-        enabled: true,
-      }
-    }),
+    tanstackStart(),
     viteReact(),
   ],
 })
